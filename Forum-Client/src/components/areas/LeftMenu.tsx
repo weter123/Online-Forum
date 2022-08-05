e5b80d2dd872ab84f192d6dd48334b9c779d0ab2
Ahmad Al Gailani <dull.x22@gmail.com>
2022-08-05 15:19:50 -0400

    testing connection to graphql

--- Forum-Client/src/components/areas/LeftMenu.tsx
+++ Forum-Client/src/components/areas/LeftMenu.tsx
@@ -2,11 +2,41 @@ import React, {useEffect,useState} from "react";
 import { useWindowDimensions } from "../../hooks/useWindowDimensions";
 import Category from "../../models/Category"
 import { getCategories } from "../../services/DataServices";
+import { gql, useQuery } from "@apollo/client";
+import { Link } from "react-router-dom";
+
+const GetAllCategories = gql `
+    query getAllCategories {
+        getAllCategories {
+            id
+            name
+        }
+    }
+`;
 const LeftMenu = () => {
+    const {loading,error,data} = useQuery(GetAllCategories);
     const {width} = useWindowDimensions();
     const [categories, setCategories] = useState<JSX.Element>(
         <div>Left Menu</div>
     )
+    useEffect(() => {
+        if(loading) {
+            setCategories(<span> Loading ...</span>);
+        }
+        else if(error){
+            setCategories(<span> Error occurred while loading categories ...</span>);
+        } else {
+            if(data && data.getAllCategories) {
+                const cats = data.getAllCategories.map((cat: any) => {
+                    return <li key= {cat.id}>
+                        <Link to={`/categorythreads/${cat.id}`}> {cat.name}</Link>
+                    </li>;
+                });
+                setCategories(<ul className= "category">{cats}</ul>)
+            }
+        }
+    },[data])
+    /*
     useEffect(()=>{
         getCategories()
         .then((categories:Array<Category>) =>{
@@ -19,7 +49,7 @@ const LeftMenu = () => {
             console.log(err);
         });
     },[]);
-
+    */
 
     if(width <= 768){
         return null;
