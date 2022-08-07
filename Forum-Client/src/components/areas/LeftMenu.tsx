287365db82fc3b1dfca3f94121bab8b344353c99
Ahmad Al Gailani <dull.x22@gmail.com>
2022-08-07 02:08:11 -0400

    cleaned up code and removed redundancies due to setting up GraphQL in App component

--- Forum-Client/src/components/areas/LeftMenu.tsx
+++ Forum-Client/src/components/areas/LeftMenu.tsx
@@ -4,52 +4,29 @@ import Category from "../../models/Category"
 import { getCategories } from "../../services/DataServices";
 import { gql, useQuery } from "@apollo/client";
 import { Link } from "react-router-dom";
+import { useAppSelector } from "../../hooks/useHooks";
 
-const GetAllCategories = gql `
-    query getAllCategories {
-        getAllCategories {
-            id
-            name
-        }
-    }
-`;
+
 const LeftMenu = () => {
-    const {loading,error,data} = useQuery(GetAllCategories);
+    const categoriesState = useAppSelector(state => state.categories);
     const {width} = useWindowDimensions();
     const [categories, setCategories] = useState<JSX.Element>(
         <div>Left Menu</div>
     )
+
     useEffect(() => {
-        if(loading) {
-            setCategories(<span> Loading ...</span>);
-        }
-        else if(error){
-            setCategories(<span> Error occurred while loading categories ...</span>);
-        } else {
-            if(data && data.getAllCategories) {
-                const cats = data.getAllCategories.map((cat: any) => {
-                    return <li key= {cat.id}>
-                        <Link to={`/categorythreads/${cat.id}`}> {cat.name}</Link>
-                    </li>;
-                });
-                setCategories(<ul className= "category">{cats}</ul>)
-            }
-        }
-    },[data])
-    /*
-    useEffect(()=>{
-        getCategories()
-        .then((categories:Array<Category>) =>{
-            const cats = categories.map((cat)=> {
-                return <li key={cat.id}>{cat.name}</li>
+        if(categoriesState.categories){
+            const cats = categoriesState.categories.map((cat: any) => {
+                return (
+                    <li key={cat.id}>
+                        <Link to={`/categorythreads/${cat.id}`}>{cat.name}</Link>
+                    </li>
+                );
             });
-            setCategories(<ul className="category">{cats}</ul>);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-    },[]);
-    */
+            setCategories(<ul className="category">{cats}</ul>)
+        }
+    }, [categoriesState]);
+    
 
     if(width <= 768){
         return null;
