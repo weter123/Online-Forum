0f561a140be7f1037ff6993dc4923a815f203fec
Ahmad Al Gailani <dull.x22@gmail.com>
2022-08-07 01:40:27 -0400

    setup GraphQL Query to get Categories from database

--- Forum-Client/src/App.tsx
+++ Forum-Client/src/App.tsx
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Home from "./components/routes/Home";
 import Thread from './components/routes/thread/Thread';
 import { Route, Routes} from 'react-router-dom';
 import UserProfile from './components/routes/UserProfile';
+import { gql, useQuery } from '@apollo/client';
+import { useAppDispatch } from './hooks/useHooks';
+import { userProfile } from './store/user/Reducers';
+import { ThreadCategories } from './store/categories/Reducer';
 
+const GetAllCategories = gql `
+    query getAllCategories {
+        getAllCategories {
+            id
+            name
+        }
+    }
+`;
 function App() {
-  
+  const { data }= useQuery(GetAllCategories);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch({
+      type: userProfile,
+      payload: {
+        id: 1,
+        userName: "testUser",
+      },
+    });
+
+    if(data && data.getAllCategories) {
+      dispatch({
+        type: ThreadCategories,
+        payload: data.getAllCategories,
+      });
+    }
+  },[ dispatch, data])
+
   return (
 
     <Routes>
@@ -16,8 +47,6 @@ function App() {
       <Route path="/userprofile/:id" element = {<UserProfile />} />
     </Routes>
 
-   
-   
   );
 }
 
