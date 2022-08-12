b1f22cb515ceab4f1f6ffac98b8d16fbc025707d
Ahmad Al Gailani <dull.x22@gmail.com>
2022-08-12 18:32:02 -0400

    refractored both components and set them up to utilzie point system ( point system not yet working)

--- Forum-Client/src/components/points/ThreadPointInline.tsx
+++ Forum-Client/src/components/points/ThreadPointInline.tsx
@@ -1,30 +1,94 @@
 import React, { FC } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHeart } from "@fortawesome/free-solid-svg-icons";
-import { ThreadPointsBarProps } from "./ThreadPointsBar";
+import { faChevronDown, faChevronUp, faHeart } from "@fortawesome/free-solid-svg-icons";
+import "./ThreadPoints.css";
+import { gql, useMutation } from "@apollo/client";
+import useUpdateThreadPoint from "../../hooks/useUpdateThreadPoints";
+const UpdateThreadItemPoint = gql`
+  mutation UpdateThreadItemPoint(
+    $threadItemId: ID! 
+    $increment: Boolean!) {
+    updateThreadItemPoint(
+      threadItemId: $threadItemId, 
+      increment: $increment)
+  }
+`;
+class ThreadPointsInLineProps {
+  points: number = 0;
+  threadId ?: string;
+  threadItemId?: string;
+  allowUpdatePoints?: boolean = false;
+  refreshThread?: () => void;
+}
 
-const ThreadPointsInline: FC<ThreadPointsBarProps> = ({
+const ThreadPointsInline: FC<ThreadPointsInLineProps> = ({
   points,
-  responseCount,
+  threadId,
+  threadItemId,
+  allowUpdatePoints,
+  refreshThread
 }) => {
+  const [execUpdateThreadItemPoint] = useMutation(UpdateThreadItemPoint);
+  const {onClickDecThreadPoint, onClickIncThreadPoint} = useUpdateThreadPoint(
+    refreshThread,
+    threadId
+  );
+
+  const onClickIncThreadItemPoint = async (
+    e:React.MouseEvent<SVGSVGElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+    console.log( " ThreadId: ", threadItemId );
+    await execUpdateThreadItemPoint({
+      variables: {
+        threadItemId,
+        increment: true,
+      }
+    });
+    refreshThread && refreshThread();
+  }
+
+  const onClickDecThreadItemPoint = async (
+    e:React.MouseEvent<SVGSVGElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+    await execUpdateThreadItemPoint({
+      variables: {
+        threadItemId,
+        increment: false,
+      }
+    });
+    refreshThread && refreshThread();
+  }
+
   return (
-    <React.Fragment>
-      <label
-        style={{
-          marginRight: ".75em",
-          marginTop: ".25em",
-        }}
+    <span className="threadpointsinline-item">
+      <div
+        className="threadpointsinline-item-btn"
+        style={{ display: `${allowUpdatePoints? "block" : "none"}` }}
       >
-        {points || 0}
         <FontAwesomeIcon
-          icon={faHeart}
-          className="points-icon"
-          style={{
-            marginLeft: ".2em",
-          }}
+          icon={faChevronUp}
+          className="point-icon"
+          onClick={threadId ? onClickIncThreadPoint : onClickIncThreadItemPoint}
         />
-      </label>
-    </React.Fragment>
+      </div>
+      {points}
+      <div
+        className="threadpointsinline-item-btn"
+        style={{ display: `${allowUpdatePoints ? "block" : "none"}` }}
+      >
+        <FontAwesomeIcon
+          icon={faChevronDown}
+          className="point-icon"
+          onClick={threadId ? onClickDecThreadPoint : onClickDecThreadItemPoint}
+        />
+      </div>
+      <div className="threadpointsinline-item-btn">
+        <FontAwesomeIcon icon={faHeart} className="points-icon" />
+      </div>
+    </span>
+   
   );
 };
 
--- Forum-Client/src/components/points/ThreadPointsBar.tsx
+++ Forum-Client/src/components/points/ThreadPointsBar.tsx
@@ -1,25 +1,60 @@
 import React, { FC } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHeart, faReplyAll } from "@fortawesome/free-solid-svg-icons";
+import { faChevronDown, faChevronUp, faHeart, faReplyAll } from "@fortawesome/free-solid-svg-icons";
 import { useWindowDimensions } from "../../hooks/useWindowDimensions";
+import useUpdateThreadPoint from "../../hooks/useUpdateThreadPoints";
 
-export interface ThreadPointsBarProps {
-  points: number;
+export class ThreadPointsBarProps {
+  points: number = 0;
   responseCount?: number;
+  threadId ?: string;
+  allowUpdatePoints?: boolean = false;
+  refreshThread?: () => void;
 }
 
 const ThreadPointsBar: FC<ThreadPointsBarProps> = ({
   points,
   responseCount,
+  threadId,
+  allowUpdatePoints,
+  refreshThread,
 }) => {
   const { width } = useWindowDimensions();
+  const {onClickDecThreadPoint, onClickIncThreadPoint} = useUpdateThreadPoint(
+    refreshThread,
+    threadId
+  );
+  
   if (width > 768) {
+    console.log("threadpintsBar points", points);
     return (
       <div className="threadcard-points">
         <div className="threadcard-points-item">
+          <div 
+            className = "threadcard-points-item-btn"
+            style = {{display:`${allowUpdatePoints} ? "block" : "none`}} >
+              <FontAwesomeIcon
+                icon={faChevronUp}
+                className =" point-icon"
+                onClick={onClickIncThreadPoint}
+              />
+          </div>
+
           {points}
-          <br />
-          <FontAwesomeIcon icon={faHeart} className="points-icon" />
+
+          <div 
+            className = "threadcard-points-item-btn"
+            style = {{display:`${allowUpdatePoints} ? "block" : "none"`}} >
+              <FontAwesomeIcon
+                icon={faChevronDown}
+                className =" point-icon"
+                onClick={onClickDecThreadPoint}
+              />
+          </div>
+          <FontAwesomeIcon 
+            icon={faHeart} 
+            className="points-icon" 
+          />
         </div>
         <div className="threadcard-points-item">
           {responseCount}
