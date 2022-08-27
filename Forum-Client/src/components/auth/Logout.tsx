66ff95a3cd0250f0dc89adafe347afcb4a0b5187
Ahmad Al Gailani <dull.x22@gmail.com>
2022-08-26 20:22:27 -0400

    changed button text to 'logout'

--- Forum-Client/src/components/auth/Logout.tsx
+++ Forum-Client/src/components/auth/Logout.tsx
@@ -54,7 +54,7 @@ const Logout: FC<ModalProps> = ({isOpen, onClickToggle}) =>{
                             className="action-btn"
                             onClick={onClickLogin}
                         >
-                        Login
+                        Logout
                         </button>
                         <button
                             style={{ marginLeft: ".5em" }}
